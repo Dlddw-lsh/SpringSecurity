3fa00911f3fad4fc5edba3f2f813e5effe6a5be0
lsh <3040114965@qq.com>
2024-04-12 21:27:04 +0800

    基于内存的用户认证

---
+++ simple-demo/src/main/java/org/lsh/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package org.lsh.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder()
+                .username("user").password("password").roles("user").build());
+        return manager;
+    }
+}
